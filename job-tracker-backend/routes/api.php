c23c4d7a7bd91a04d77b4b8a27888b822f741496
Abdelghani Elothmani <elothmani1111@gmail.com>
2026-05-30 09:34:40 +0100

    temporary remove auth

--- job-tracker-backend/routes/api.php
+++ job-tracker-backend/routes/api.php
@@ -4,14 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\JobApplicationController;
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-
-    Route::get('/jobs', [JobApplicationController::class, 'index']);
-    Route::post('/jobs', [JobApplicationController::class, 'store']);
-    Route::put('/jobs/{id}', [JobApplicationController::class, 'update']);
-    Route::delete('/jobs/{id}', [JobApplicationController::class, 'destroy']);
-});
+Route::get('/jobs', [JobApplicationController::class, 'index']);
+Route::post('/jobs', [JobApplicationController::class, 'store']);
+Route::put('/jobs/{id}', [JobApplicationController::class, 'update']);
+Route::delete('/jobs/{id}', [JobApplicationController::class, 'destroy']);
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
